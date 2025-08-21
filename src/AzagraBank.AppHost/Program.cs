30fca4f9623c062cae0a3f011bd1454957200318
Kirill Perepechkin <perepechkin_2002@mail.ru>
2025-08-21 11:58:46 +0300

    added kafka and IEventBus

--- src/AzagraBank.AppHost/Program.cs
+++ src/AzagraBank.AppHost/Program.cs
@@ -1,21 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("pgsql")
+var postgres = builder.AddPostgres("PostgreSQL")
     .WithPgAdmin()
     .WithLifetime(ContainerLifetime.Persistent);
 
 var azagraBankDb = postgres.AddDatabase("azagra-bank-db");
 
-var azagraBankCache = builder.AddRedis("azagra-bank-cache");
+var azagraBankCache = builder.AddRedis("Redis");
 
-var api = builder.AddProject<Projects.AzagraBank_ApiService>("azagrabank-apiservice")
-    .WithReference(azagraBankDb)
-    .WaitFor(azagraBankDb)
+var kafka = builder.AddKafka("Kafka");
+
+var api = builder.AddProject<Projects.AzagraBank_ApiService>("Api")
+    .WithReference(kafka)
+    .WaitFor(kafka)
     .WithReference(azagraBankCache)
     .WaitFor(azagraBankCache)
     .WithExternalHttpEndpoints();
 
-var client = builder.AddNpmApp("react", "../AzagraBank.Client")
+var client = builder.AddNpmApp("Client", "../AzagraBank.Client")
     .WithReference(api)
     .WaitFor(api)
     .WithHttpEndpoint(port: 5173, targetPort: 5174, env: "PORT")
